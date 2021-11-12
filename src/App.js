a77ca3888b4d5a10197dcb594b423db9c3f254fe
Claire List <claire.list2501@gmail.com>
2021-11-12 11:35:38 -0700

    app framework done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,  {useState,useEffect} from "react";
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 
 import Order from './components/Order';
+import Home from './components/Home';
 
 // import * as yup from 'yup';
 // import schema from './formSchema'; //--to build out!
@@ -19,11 +20,26 @@ const initOrderData= {
     special: ''
 }
 
+const initFormErrors = {
+  name:''
+}
+
 const App = () => {
+
+  //DATA AKA STATE SECTION
+
+  const [orderData,setOrderData] = useState(initOrderData);
+  const [formErrors,setFormErrors] = useState(initFormErrors);
+  const [orders,setOrders] = useState([]); 
+
+  //LOGIC SECTIONR
+  //needful: a doChange and a doSubmit func
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>ORDER NOW! CONSUME ! CONSUME ! CONSUME !</p>
+      
 
       <Router>
           <nav className='navbar'>
@@ -31,8 +47,10 @@ const App = () => {
             <Link to='/'>Take me HOME!</Link>
           </nav>
       {/*ROUTE STUFF HERE*/}
-      {/* <Route exact path='/' component={Home}/> */}
-      <Route path='/pizza' component={Order}/>
+      <Route exact path='/' component={Home}/>
+      <Route path='/pizza'>
+        <Order />
+      </Route>
 
       </Router>
     </>
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className='home-wrapper'>
+            <p>ORDER NOW! CONSUME ! CONSUME ! CONSUME !</p>
+        </div>
+    )
+}
+
+export default Home;
