07fee49b7a775e2f916080666aa1015324d8e4c0
Claire List <claire.list2501@gmail.com>
2021-11-12 10:25:41 -0700

    one test passing yay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+//import Home from './components/Home';
+//import Order from './components/Order';
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>I will put things here!</p>
+      <p>ORDER NOW! CONSUME ! CONSUME ! CONSUME !</p>
+      
+      <Router>
+          <nav className='navbar'>
+            <Link id='order-pizza' to='/pizza'>ORDER a PIE !</Link>
+            <Link to='/'>Take me HOME!</Link>
+          </nav>
+      {/*ROUTE STUFF HERE*/}
+      {/* <Route exact path='/' component={Home}/> */}
+      {/* <Route path='/pizza' component={Order}/> */}
+
+      </Router>
     </>
   );
 };
