6db2d87d314b8b502bf7f95604850900ae2bf9fa
Claire List <claire.list2501@gmail.com>
2021-11-12 10:45:55 -0700

    building out a framework

---
+++ src/components/Order.js
@@ -0,0 +1,36 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+
+// const orderData= {
+//     name: '',
+//     size: '', //DROPDOWN
+
+//     pepperoni: 0, //NOT INTS!
+//     olives: 0,
+//     mushrooms: 0,
+//     pineapple: 0,
+
+//     special: ''
+// }
+
+const Order = (props) => {
+
+    //STATE goeth here
+
+    //build change functionality
+
+    const handleChange=()=>{
+
+    }
+
+    const handleSubmit=()=>{
+
+    }
+
+    return (
+        <div id='pizza-form'>
+        </div>
+    )
+}
+
+export default Order;
